a71f59611caf6e278ac1882c9a2f7e8b36498306
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-12-17 14:23:14 +0200

    Don't 'make clean' in CI, it's already clean

--- centos-7-amd64/test.sh
+++ centos-7-amd64/test.sh
@@ -2,7 +2,6 @@
 source /vpy3/bin/activate
 cd /Pillow
 export DISPLAY=:99.0
-make clean
 make install-coverage
 python3 -c "from PIL import Image"
 /usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
